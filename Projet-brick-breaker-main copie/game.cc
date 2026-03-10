54060f31a67ab2364dd4f43b1c166de8893188f4
alice-ndrs <alice.andros@epfl.ch>
2026-03-10 23:29:37 +0100

    ajout méthode de lecture de fichier

--- Projet-brick-breaker-main copie/game.cc
+++ Projet-brick-breaker-main copie/game.cc
@@ -1,6 +1,25 @@
 #include <iostream>
 #include "game.h"
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+void game::getLevel(const string& filename){ // méthode de lecture de fichier
+    ifstream file(filename);
+
+    if (file.fail()) {
+        // afficher mess erreur "Erreur ouverture fichier"
+    }
+
+    string line;
+    while (getline(file>>ws, line)) {
+        istringstream data(line);
+        // lire les données, créer les objets nécessaires
+    }
+}
+
+// getLevel est responsable de lire le fichier et vérifier que son contenu est correct.
+// Les classes Brick, Ball, Paddle, etc. sont responsables de vérifier que leurs paramètres sont valides.
 
 
 game::game (int lives,int score):
--- Projet-brick-breaker-main copie/game.h
+++ Projet-brick-breaker-main copie/game.h
@@ -1,10 +1,12 @@
 #ifndef game_H
 #define game_H
+#include <string>
 
 
 class game{
     public:
         game (int lives,int score);
+        void getLevel(const std::string& filename); // déclaration de la fn de lecture de fichier
     private:
         int lives;
         int score;
