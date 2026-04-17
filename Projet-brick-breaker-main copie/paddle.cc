f7600bf3324198622d94f584c0d0679437dca2fd
Florine-sar <florinesarrazyn@icloud.com>
2026-04-17 13:13:31 +0200

    message paddle

--- Projet-brick-breaker-main copie/paddle.cc
+++ Projet-brick-breaker-main copie/paddle.cc
@@ -20,11 +20,13 @@ int Paddle::check_Paddle() const
                         circle.center.y * circle.center.y; 
                         
     if (circle.center.y > 0 || dx_squared < 0) {
+        cout << message::paddle_outside(circle.center.x, circle.center.y);
         return 1;
     }
     
     // Une partie du cercle doit être visible
     if (circle.center.y + circle.r <= 0) { 
+        cout << message::paddle_outside(circle.center.x, circle.center.y);
         return 1;
     }
 
@@ -33,6 +35,7 @@ int Paddle::check_Paddle() const
     double right = circle.center.x + dx;
 
     if (left < 0 || right > arena_size) {
+        cout << message::paddle_outside(circle.center.x, circle.center.y);
         return 1;
     }
 
